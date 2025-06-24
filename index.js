cbb1f9c641c1242c1daba803f7f8af030276abac
Yash <yashwadkar079@gmail.com>
2025-06-25 01:37:00 +0530

    CHANGE CORS POLICES

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 require("dotenv").config();
 const connectToMongo = require("./db");
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const transactionRoutes = require("./routes/transactionRoutes");
 
 connectToMongo();
 const app = express();
 const port = 5000;
 
-// middleware for sending JSON body to DB
 app.use(express.json());
 
 const allowedOrigins = ["http://localhost:5173", "https://nd-task.vercel.app"];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy does not allow this origin"), false);
       }
-      return callback(null, true);
     },
-    methods: ["POST", "GET", "DELETE", "PUT"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
-// Available Routes
+// Routes
 app.use("/api/transactions", transactionRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World from server");
 });
+
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
